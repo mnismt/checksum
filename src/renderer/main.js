3f950158df37a2c2be050a1b3dbd2b9ca0db2d30
Thanh Minh <thanhgamedev@gmail.com>
2018-03-12 22:47:48 +0700

    add: vuetify

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 
 import App from './App'
 import router from './router'
+import Vuetify from 'vuetify'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
+Vue.use(Vuetify)
+import 'vuetify/dist/vuetify.min.css'
 
 /* eslint-disable no-new */
 new Vue({
