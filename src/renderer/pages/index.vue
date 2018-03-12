8a79be07053703a4319988d6f9f60701dc1fde35
Thanh Minh <thanhgamedev@gmail.com>
2018-03-12 22:59:04 +0700

    add: index page

---
+++ src/renderer/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card>
+    <v-card-text>
+      <checksum />
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import Checksum from '@/components/checksum/Checksum'
+
+export default {
+  components: {
+    Checksum
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -3,8 +3,12 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
+function load (page) {
+  return require(`@/pages/${page}`).default
+}
+
 export default new Router({
   routes: [
-    
+    { path: '/', component: load('index') }
   ]
 })
