e5077922d90b2820e3bf0e91534f1f97bbfb9ef3
Thanh Minh <thanhgamedev@gmail.com>
2018-03-12 22:57:52 +0700

    add: app layout

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+  <v-app id="inspire">
+    <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-title>Checksum</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <router-view />
+      </v-container>
+    </v-content>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2017</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-  export default {
-    name: 'checksum'
-  }
+export default {
+}
 </script>
-
-<style>
-  /* CSS */
-</style>
